37a80599ee8380e5709059e9c85b4cd1e066e7e0
Eric Howard <EricHoward@live.ca>
2024-09-09 00:44:17 -0400

    changed config

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://zurtix.github.io',
   integrations: [mdx(), sitemap(), tailwind({
     applyBaseStyles: false
   }), react()],
