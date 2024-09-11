4a1d7d6c96087df9effeb36a5c492a9eb448db38
Eric Howard <EricHoward@live.ca>
2024-09-10 20:53:54 -0400

    Updated content and badges

--- src/content/blog/specterpoint.md
+++ src/content/blog/specterpoint.md
@@ -1,9 +1,9 @@
 ---
 title: 'SpecterPoint'
 description: 'The Rust C2 Server'
-pubDate: 'Sep 09 2024'
+pubDate: 'Sep 10 2024'
 heroImage: '/specterpoint/specterpoint.jpg'
-tags: ['rust', 'c2', 'malware']
+tags: ['rust', 'c2', 'malware', 'under cunstruction']
 ---
 
 Under construction
--- src/content/config.ts
+++ src/content/config.ts
@@ -10,7 +10,8 @@ const blog = defineCollection({
     pubDate: z.coerce.date(),
     updatedDate: z.coerce.date().optional(),
     heroImage: z.string().optional(),
-    tags: z.array(z.string()).optional().default([])
+    tags: z.array(z.string()).optional().default([]),
+    underConstruction: z.boolean().optional().default(true)
   }),
 });
 
